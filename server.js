741e87d9555d7282b1a848c8bdb368065d7477d1
wittenator <wittenator@yahoo.de>
2018-01-31 16:42:54 +0100

    Shortened server routing

--- server.js
+++ server.js
@@ -13,96 +13,34 @@ var clientSecret = process.env.CLIENT_SECRET || secrets.clientSecret;
 
 // Starts server
 app.listen(PORT, function () {
-    console.log("Relay listening on port " + PORT);
+    console.log(`Relay listening on port ${PORT}`);
 });
 
-//Juniter Endpoint
-app.post('/juniter', urlencodedParser, function(req, res) {
-  //relay message
-  const dest = 'juniter'
-  const message = buildMessageBody(req.body)
-  sendMessageToSlack(dest, message)
-  //send acknowledgement
-  sendAcknowledgement(res,req,dest)
-});
-
-//Vorstand Endpoint
-app.post('/vorstand', urlencodedParser, function(req, res) {
-  const dest = 'vorstand'
-  sendMessageToSlack(dest, buildMessageBody(req.body))
-  sendAcknowledgement(res,req,dest)
-});
-
-//RL-Runde Endpoint
-app.post('/rlrunde', urlencodedParser, function(req, res) {
-  const dest = 'rl-runde'
-  sendMessageToSlack(dest, buildMessageBody(req.body))
-  sendAcknowledgement(res,req,dest)
-});
-
-//HR-Endpoint
-app.post('/hr', urlencodedParser, function(req, res) {
-  const dest = 'hr'
-  sendMessageToSlack(dest, buildMessageBody(req.body))
-  sendAcknowledgement(res,req,dest)
-});
-
-//IN-Endpoint
-app.post('/in', urlencodedParser, function(req, res) {
-  const dest = 'in'
-  //const message = buildMessageBody(req.body)
-  //sendMessageToSlack('vorstand', message)
-  //send acknowledgement
-  res.send("relay for IN has not been configured yet")
-  res.status(200).end()
-  console.log("did not relay from " + req.body.user_name +" to in : " + req.body.text)
-});
-
-//QM-Endpoint
-app.post('/qm', urlencodedParser, function(req, res) {
-  //relay message
-  const dest = 'juniter'
-  //const message = buildMessageBody(req.body)
-  //sendMessageToSlack('vorstand', message)
-  //send acknowledgement
-  res.send("relay for QM has not been configured yet")
-  res.status(200).end()
-  console.log("did not relay from " + req.body.user_name +" to qm : " + req.body.text)
-});
-
-//RM-Endpoint
-app.post('/rm', urlencodedParser, function(req, res) {
-  //relay message
-  const dest = 'rm'
-  //const message = buildMessageBody(req.body)
-  //sendMessageToSlack('vorstand', message)
-  //send acknowledgement
-  res.send("relay for RM has not been configured yet")
-  res.status(200).end()
-  console.log("did not relay from " + req.body.user_name +" to rm : " + req.body.text)
-});
-
-//PR-Endpoint
-app.post('/pr', urlencodedParser, function(req, res) {
-  //relay message
-  const dest = 'pr'
-  //const message = buildMessageBody(req.body)
-  //sendMessageToSlack('vorstand', message)
-  //send acknowledgement
-  res.send("relay for PR has not been configured yet")
-  res.status(200).end()
-  console.log("did not relay from " + req.body.user_name +" to pr : " + req.body.text)
+//Ressort Endpoint
+// req.gruppe is the url path parameter
+app.post('/:gruppe', urlencodedParser, function(req, res) {
+  if (Object.keys(secrets.addresses).includes(req.params.gruppe)) {
+    sendMessageToSlack(req.params.gruppe, buildMessageBody(req.body))
+    sendAcknowledgement(res,req,req.params.gruppe)
+  }
+  else {
+    res.send(`relay for ${req.param.gruppe} has not been configured yet`)
+       .status(200)
+       .end()
+    console.log(`did not relay from ${req.body.user_name} to in : ${req.body.text}`)
+  }
 });
 
 //setting up message
 function buildMessageBody(request){
-  return { "text": "relayed from " + request.user_name + " : " + request.text }
+  return {'text': `relayed from ${request.user_name} : ${request.text}` }
 }
 
 function sendAcknowledgement(res,req,dest) {
-  res.send("relayed message to " + dest + " :+1:")
-  res.status(200).end()
-  console.log("did relay from " + req.body.user_name +" to "+ dest +" : " + req.body.text)
+  res.send(`relayed message to ${dest} :+1:`)
+     .status(200)
+     .end()
+  console.log(`did relay from ${req.body.user_name} to  ${dest} : ${req.body.text}`)
 }
 
 // setting up Post request for relaying message
